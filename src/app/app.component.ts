f7048a7251eb807ee80d00bf97e447a0fb47364b
Gaurav-Sanchania <gauravinfinnium@gmail.com>
2025-01-09 18:35:49 +0530

    standalone project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularTutorial';
+
+  changeTitle() {
+    this.title = 'Gaurav Sanchania';
+  }
+
+  ngOnInit() {
+    // It will be called when the component is initialized
+    // It is a lifecycle hook
+
+    // this.changeTitle();
+  }
 }
